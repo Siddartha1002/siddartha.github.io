4758f0dae0211da7d64e70415b6d0f401034a821
Siddartha1002 <siddarthakomari@gmail.com>
2024-10-28 19:58:24 -0500

    updated code for chanegs

--- Lab-03/script.js
+++ Lab-03/script.js
@@ -1,5 +1,5 @@
 function calculateTip() {
-    const amount = document.getElementById('amount').value;
+    let amount = document.getElementById('amount').value;
     const tipPercentage = parseFloat(document.getElementById('tipPercentage').value);
     const currency = document.getElementById('currency').value;
     const errorMessage = document.getElementById('errorMessage');
@@ -8,13 +8,14 @@ function calculateTip() {
     errorMessage.innerText = '';
 
     // Validate the amount: it should be a non-negative number
-    if (isNaN(amount)) {
+    if (isNaN(amount) ||  amount < 0) {
         errorMessage.innerText = 'Please enter a valid amount (non-negative number).';
         document.getElementById('tipAmount').value = '';
         document.getElementById('totalAmount').value = '';
         document.getElementById('displayTipPercentage').value = '';
         return;
-    }
+    }   
+    console.log(amount);
     amount = parseFloat(amount);
     // Calculate the tip amount in USD
     const tipAmountUSD = (amount * tipPercentage) / 100;
